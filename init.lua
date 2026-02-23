7b9dfc17911c1332356ab102dcd71b940f1d0095
plainduck67 <plainduck67@gmail.com>
2026-02-23 12:52:57 -0700

    add fuzzy finder

--- init.lua
+++ init.lua
@@ -108,6 +108,18 @@ if not vim.loop.fs_stat(lazypath) then
     },
 
     {
+      "nvim-telescope/telescope.nvim",
+      tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      end
+    },
+
+    {
       "neovim/nvim-lspconfig",
       config = function()
       local lspconfig = require("lspconfig")
