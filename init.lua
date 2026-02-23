92ea63d46c3bb6947fce3d66fd21386a9428e2ee
plainduck67 <plainduck67@gmail.com>
2026-02-23 02:35:18 -0700

    add autopairs and line numbers

--- init.lua
+++ init.lua
@@ -5,51 +5,60 @@ if not vim.loop.fs_stat(lazypath) then
     "https://github.com/folke/lazy.nvim.git",
     lazypath,
   })
-end
-vim.opt.rtp:prepend(lazypath)
+  end
+  vim.opt.rtp:prepend(lazypath)
 
+  vim.wo.number = true
+  vim.wo.relativenumber = true
+  vim.opt.shiftwidth = 2
+  vim.opt.tabstop = 2
+  vim.opt.expandtab = true
+  vim.opt.expandtab = true
+  vim.opt.cindent = true
 
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.expandtab = true
-vim.opt.cindent = true
+  vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true })
+  vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
+  vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true })
+  vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true })
+  vim.keymap.set("n", "H", "gT", { noremap = true, silent = true })
+  vim.keymap.set("n", "L", "gt", { noremap = true, silent = true })
 
-vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true })
-vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
-vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true })
-vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true })
-vim.keymap.set("n", "H", "gT", { noremap = true, silent = true })
-vim.keymap.set("n", "L", "gt", { noremap = true, silent = true })
+  vim.opt.termguicolors = true
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = " "
 
-vim.opt.termguicolors = true
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.keymap.set("n", "<C-q>", "<cmd>NvimTreeToggle<CR>", { silent = true, noremap = true })
---
-require("lazy").setup({
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
+  vim.keymap.set("n", "<C-q>", "<cmd>NvimTreeToggle<CR>", { silent = true, noremap = true })
+  --
+  require("lazy").setup({
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      lazy = false,
+      priority = 1000,
+      config = function()
       require("catppuccin").setup({
         flavour = "frappe",
         integrations = { treesitter = true, nvimtree = true, lualine = true },
       })
       vim.cmd.colorscheme("catppuccin-frappe")
-    end,
-  },
+      end,
+    },
 
-  { "nvim-tree/nvim-web-devicons", lazy = false },
+    { "nvim-tree/nvim-web-devicons", lazy = false },
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
-    config = function()
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+      require("nvim-autopairs").setup({})
+      end,
+    },
+
+    {
+      "nvim-tree/nvim-tree.lua",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      lazy = false,
+      config = function()
       require("nvim-tree").setup({
         sort_by = "case_sensitive",
 
@@ -78,54 +87,54 @@ require("lazy").setup({
         },
 
         on_attach = function(bufnr)
-          local api = require("nvim-tree.api")
+        local api = require("nvim-tree.api")
 
-          local function opts(desc)
-            return {
-              desc = "nvim-tree: " .. desc,
-              buffer = bufnr,
-              noremap = true,
-              silent = true,
-              nowait = true,
-            }
-          end
+        local function opts(desc)
+        return {
+          desc = "nvim-tree: " .. desc,
+          buffer = bufnr,
+          noremap = true,
+          silent = true,
+          nowait = true,
+        }
+        end
 
-          vim.keymap.set("n", "<CR>", api.node.open.tab, opts("Open: New Tab"))
+        vim.keymap.set("n", "<CR>", api.node.open.tab, opts("Open: New Tab"))
 
-          vim.keymap.set("n", "o", api.node.open.tab, opts("Open: New Tab"))
+        vim.keymap.set("n", "o", api.node.open.tab, opts("Open: New Tab"))
         end,
       })
-    end,
-  },
+      end,
+    },
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
       local lspconfig = require("lspconfig")
       lspconfig.clangd.setup({
         cmd = { "clangd" },
         filetypes = { "c", "cpp", "objc", "objcpp" },
         on_attach = function(client, bufnr)
-          local opts = { noremap = true, silent = true }
-          vim.keymap.set("n", "gd", function()
-            vim.cmd("tab split")
-            vim.lsp.buf.definition()
-          end, { noremap = true, silent = true })
-          vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-          vim.keymap.set("n", "<leader>?", vim.diagnostic.open_float)
+        local opts = { noremap = true, silent = true }
+        vim.keymap.set("n", "gd", function()
+        vim.cmd("tab split")
+        vim.lsp.buf.definition()
+        end, { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        vim.keymap.set("n", "<leader>?", vim.diagnostic.open_float)
         end,
       })
-    end,
-  },
+      end,
+    },
 
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
-    config = function()
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      lazy = false,
+      config = function()
       require("lualine").setup({
         options = { theme = "catppuccin", section_separators = "", component_separators = "" },
       })
-    end,
-  },
-})
+      end,
+    },
+  })
